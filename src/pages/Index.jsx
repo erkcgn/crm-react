47417f5d733cb2514868972845cf21d0bf89df1f
erk.cgn <erikac.ec80@gmail.com>
2023-08-16 14:47:39 -0300

    cree tabla para mostrar el componente clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-key */
 import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
 export function loader(){
     const clientes = [
@@ -45,14 +47,35 @@ export function loader(){
 function Index() {
 
     const clientes = useLoaderData();
-    console.log(clientes)
 
   return (
     <>
-        <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
-        <p className="mt-3">Administra tus Clientes</p>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus Clientes</p>
+
+      {clientes.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Clientes</th>
+              <th className="p-2">Contactos</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map(cliente => (
+              <Cliente
+                cliente={cliente}
+                key={cliente.id}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10"> No hay Clientes aún</p>
+      )}
     </>
-  )
+  );
 }
 
 export default Index
