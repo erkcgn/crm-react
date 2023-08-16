69ca9fcf1fe3c2bbc3624f54496988f6b90a4aa5
erk.cgn <erikac.ec80@gmail.com>
2023-08-16 15:45:55 -0300

    agregando validaciones al form

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,15 +1,32 @@
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useActionData } from "react-router-dom";
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 export async function action({request}){
     const formData = await request.formData()
+    const datos = Object.fromEntries(formData)
 
-    console.log(formData)
+    //validacion
+    const errores = []
+    if(Object.values(datos).includes('')){
+        errores.push('Todos los campos son obligatorios')
+    }
+    
+    //retornar datos si hay errores
+    if(Object.keys(errores).length){
+        return errores
+    }
+
     return "desde action"
 }
 
 function NuevoCliente() {
+
+    const errores = useActionData()
+
     const navigate = useNavigate()
+
+    console.log(errores)
 
   return (
     <>
@@ -27,6 +44,8 @@ function NuevoCliente() {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+        {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
         <Form
             method="POST"            
         >
