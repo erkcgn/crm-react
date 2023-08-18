4017c99e986d46afa4d6a9d39b658272f1e1813c
erk.cgn <erikac.ec80@gmail.com>
2023-08-18 10:44:03 -0300

    se crea funcion actualizarCliente, se adapta el action para edicion

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-control-regex */
 /* eslint-disable react-refresh/only-export-components */
-import { useNavigate, Form, useActionData, useLoaderData } from "react-router-dom"
-import { obtenerCliente } from "../data/Clientes"
+import { useNavigate, Form, useActionData, useLoaderData, redirect } from "react-router-dom"
+import { obtenerCliente, actualizarCliente } from "../data/Clientes"
 import Error from "../components/Error"
 import Formulario from "../components/Formulario"
 
@@ -17,7 +18,34 @@ export async function loader({params}){
 }
 
 export async function action({request,params}){
-    
+  const formData = await request.formData();
+  const datos = Object.fromEntries(formData);
+
+  const email = formData.get("email");
+
+  //validacion
+  const errores = [];
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios");
+  }
+
+  // eslint-disable-next-line no-control-regex, no-useless-escape
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  if (!regex.test(email)) {
+    errores.push("El email no es valido");
+  }
+
+  //retornar datos si hay errores
+  if (Object.keys(errores).length) {
+    return errores;
+  }
+
+  //actualizar el cliente
+  await actualizarCliente(params.clienteId,datos);
+
+  return redirect('/');
 }
 
 function EditarCliente() {
@@ -54,7 +82,7 @@ function EditarCliente() {
           <input
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-            value="Registrar Cliente"
+            value="Guardar Cambios"
           />
         </Form>
       </div>
